1d524ac1ff4c1cac02230a85430f888e89ba660a
kritical0613 <kulishan0613@gmail.com>
2022-06-23 11:29:56 -0500

    feat: header responsive

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,22 @@
-import meat from 'app/assets/header/meat.png'
-import logo from 'app/assets/header/logo.png'
-import loupe from 'app/assets/header/loupe.svg'
-import user from 'app/assets/header/user.svg'
-import cart from 'app/assets/header/cart.svg'
+import { useState } from 'react'
+import meat from 'assets/header/meat.svg'
+import logo from 'assets/header/logo.png'
+import loupe from 'assets/header/loupe.svg'
+import user from 'assets/header/user.svg'
+import cart from 'assets/header/cart.svg'
 
 import './index.scss'
 
 const Header: React.FC = () => {
+  const [isOpen, setOpen] = useState(false)
+
   return (
     <div className="header">
-      <a href='#meat'><img src={meat} alt="meat" /></a>
-      <img src={logo} alt="logo" />
-      <div className="menu">
+      <div className="logo">
+        <a href='#meat' className="meat" onClick={() => setOpen(!isOpen)}><img src={meat} alt="meat" width={28} height={28} /></a>
+        <img className="logo-image" src={logo} alt="logo" />
+      </div>
+      <div className={`menu ${isOpen ? "open" : ""}`}>
         <a href='#men'>Men's</a>
         <a href='#women'>Women's</a>
         <a href='#accessories'>Accessories</a>
